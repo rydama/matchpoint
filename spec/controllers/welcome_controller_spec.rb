0d172e9c231760c6975b7d9466f0b8b3526b3859
Ryan Manwiller <rydama@gmail.com>
2017-02-25 10:27:01 -0800

    Welcome controller spec

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,5 +1,21 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe WelcomeController, type: :controller do
+describe WelcomeController, type: :controller do
+  describe "GET index" do
+    context "when not signed in" do
+      it "renders the index template" do
+        get :index
+        expect(response).to render_template "index"
+      end
+    end
 
+    context "when signed in" do
+      before { sign_in }
+
+      it "redirects to the home page" do
+        get :index
+        expect(response).to redirect_to home_path
+      end
+    end
+  end
 end
