9a17f8dc5065eaa97eb5d9844d008765e232a4af
Ryan Manwiller <rydama@gmail.com>
2017-02-25 17:39:57 -0800

    Fix registration spec

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -10,15 +10,20 @@ describe RegistrationsController, type: :controller do
     end
 
     context "when signed in" do
+      let(:tournament) { create(:tournament) }
       before do
         sign_in
       end
 
-      it "creates a registration" do
-        post :create, params: { registration: { tournament_id: create(:tournament).id } }
-        expect(response).to be_success
+      it "creates a registration and redirects home" do
+        post :create, params: { tournament_id: tournament.id }
+        expect(response).to redirect_to home_path
+      end
+
+      it "creates a registration and redirects to the given redirect_to param" do
+        post :create, params: { tournament_id: tournament.id, redirect_to: tournament_path(tournament) }
+        expect(response).to redirect_to tournament_path(tournament)
       end
     end
   end
 end
-
